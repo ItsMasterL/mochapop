0f48149d7ce1dde4783c7a9c3f998a07a2b6a9ad
Master L <kingcreeper7777@gmail.com>
2021-12-21 02:49:10 -0800

    Initial commit

---
+++ Assets/Pop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pop : MonoBehaviour
+{
+    public Animator anim;
+    public AudioSource pop;
+    public AudioSource unpop;
+    public int popcount = 0;
+    public GUIStyle style;
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.Play("pop");
+            pop.Play();
+            popcount += 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.Play("not pop");
+            unpop.Play();
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), popcount.ToString(), style);
+    }
+}
