92fd9bb20f66ce4846d7c6d8fa18678f54e2b711
Master L <kingcreeper7777@gmail.com>
2022-05-25 15:48:52 -0700

    Added invite link, also testing webhook

---
+++ Assets/JoinDiscord.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinDiscord : MonoBehaviour
+{
+    public void Join()
+    {
+        Application.OpenURL("https://discord.gg/rfgw3s3hFF");
+    }
+}
